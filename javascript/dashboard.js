7edde4fc171e667fd08a0ca645691dc6cc0ea93c
negatiwe <73194921+negatiwe@users.noreply.github.com>
2025-03-30 22:17:25 +0300

    Fixed some bugs and added OTP Verification front-end

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -5,3 +5,22 @@ document.querySelector(".category-title").addEventListener("mouseenter", () => {
 document.querySelector(".category-title").addEventListener("mouseleave", () => {
     document.querySelector(".arrow-icon").classList.remove("hover");
 });
+
+$(document).ready(function() {
+    // Adăugăm clasa hidden în CSS
+    $('<style>').prop('type', 'text/css').html('.hidden { display: none; }').appendTo('head');
+
+    $('#homepage').click(function() {
+        $('.homepage-content').removeClass('hidden');
+        $('.members-content').addClass('hidden');
+    });
+
+    $('#members').click(function() {
+        $('.members-content').removeClass('hidden');
+        $('.homepage-content').addClass('hidden');
+    });
+
+    // Ascundem members-content inițial
+    $('.members-content').addClass('hidden');
+});
+
