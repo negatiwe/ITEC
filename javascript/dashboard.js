0d5bdf1a8aa243f07789fe3c2be34e495b1090b8
ADRIAN PETRIȘOR <163734593+adrianpetrisor@users.noreply.github.com>
2025-04-04 16:02:46 +0300

    Added - Your Account; Created - Account Setting - still blank;

--- javascript/dashboard.js
+++ javascript/dashboard.js
@@ -22,3 +22,50 @@ $(document).ready(function() {
     $('.members-content').addClass('hidden');
 });
 
+$(document).ready(function () {
+    $('.dashboard-view-notifications').on('click', function () {
+        const box = $('#notification-box');
+
+        if (box.hasClass('hidden')) {
+            box.removeClass('hidden');
+
+            anime({
+                targets: '#notification-box',
+                translateY: [-30, 0],
+                opacity: [0, 1],
+                duration: 400,
+                easing: 'easeOutQuad'
+            });
+
+        } else {
+            anime({
+                targets: '#notification-box',
+                translateY: [0, -30],
+                opacity: [1, 0],
+                duration: 300,
+                easing: 'easeInQuad',
+                complete: function () {
+                    box.addClass('hidden');
+                }
+            });
+        }
+    });
+
+    $(document).on('click', function (e) {
+        const box = $('#notification-box');
+        if (!$(e.target).closest('#notification-box, .dashboard-view-notifications').length) {
+            if (!box.hasClass('hidden')) {
+                anime({
+                    targets: '#notification-box',
+                    translateY: [0, -30],
+                    opacity: [1, 0],
+                    duration: 300,
+                    easing: 'easeInQuad',
+                    complete: function () {
+                        box.addClass('hidden');
+                    }
+                });
+            }
+        }
+    });
+});
